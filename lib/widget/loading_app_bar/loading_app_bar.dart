708e0bc6d3470f7ce16b8e7ff83e17af44d2e73b
Chronm <info@chronm.dev>
2020-05-18 12:53:30 +0200

    refactor(ui): autosize text in appbar

--- lib/widget/loading_app_bar/loading_app_bar.dart
+++ lib/widget/loading_app_bar/loading_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:Blackout/bloc/main/main_bloc.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -48,8 +49,9 @@ class _LoadingAppBarState<B extends Bloc<dynamic, S>, S> extends State<LoadingAp
         child: SizedBox(
           height: 56,
           child: Center(
-            child: Text(
+            child: AutoSizeText(
               widget.title ?? widget.titleResolver(state),
+              maxLines: 1,
             ),
           ),
         ),
