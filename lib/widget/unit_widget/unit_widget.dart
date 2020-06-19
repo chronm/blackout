915df06b1b5d8b88e20964169a8b6bf730b95529
Chronm <info@chronm.dev>
2020-06-19 16:52:55 +0200

    feat(units): internationalize units

--- lib/widget/unit_widget/unit_widget.dart
+++ lib/widget/unit_widget/unit_widget.dart
@@ -1,3 +1,4 @@
+import 'package:Blackout/generated/l10n.dart';
 import 'package:Blackout/models/unit/unit.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -27,14 +28,30 @@ class _UnitWidgetState extends State<UnitWidget> {
     _unit = widget.initialUnit ?? UnitEnum.values[0];
   }
 
+  String enumToString(BuildContext context, UnitEnum unit) {
+    switch (unit) {
+      case UnitEnum.unitless:
+        return "------";
+      case UnitEnum.weight:
+        return S.of(context).UNITENUM_WEIGHT;
+    }
+    return "";
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<UnitEnum> enums = List.from(UnitEnum.values)..sort((a, b) => enumToString(context, a).compareTo(enumToString(context, b)));
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Center(
           child: DropdownButton<UnitEnum>(
-            items: UnitEnum.values.map((u) => DropdownMenuItem(value: u, child: Text(describeEnum(u)))).toList(),
+            items: enums
+                .map((u) => DropdownMenuItem(
+                      value: u,
+                      child: Text(enumToString(context, u)),
+                    ))
+                .toList(),
             onChanged: (unit) {
               setState(() {
                 _unit = unit;
