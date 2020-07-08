a3a071fff4eda35389b87b4f88b773eaad8d225b
Chronm <info@chronm.dev>
2020-07-08 16:21:28 +0200

    refactor(website): move from gh-pages to master branch

---
+++ docs/js/tiny-slider.js
@@ -0,0 +1,9 @@
+var tinySlider = tns({
+    loop: false,
+    container: '.slider',
+    items: 2,
+    autoplay: false,
+    controls: false,
+    navContainer: ".slider-dots",
+    rewind: true
+});
